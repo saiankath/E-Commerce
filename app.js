9b032d5b9d6e6fb46a24bd57064abb049540d2ef
saiankath <mesaiankath@gmail.com>
2020-08-11 18:38:47 -0300

    backend project

---
+++ app.js
@@ -0,0 +1,53 @@
+require("dotenv") .config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+// My routes
+const authRoutes = require("./routes/auth")
+const userRoutes = require("./routes/user")
+const categoryRoutes = require("./routes/category")
+const productRoutes = require("./routes/product")
+const orderRoutes = require("./routes/order")
+
+
+
+
+
+// DB connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("DB CONNECTED");
+});
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+// My routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+
+
+
+
+
+// PORT
+const port = process.env.PORT || 8000;
+
+// Starting a Server
+app.listen(port, () => {
+    console.log(`app is running at ${port}`);
+})
